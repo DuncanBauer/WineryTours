4d70a6dc869f79171595a6c685a380ad27d8a43a
Erick <Erick@CompSlime.student.saddleback.edu>
2015-02-26 20:45:46 -0800

    This function finds the smallest number in a vector of floats that needs to be passed in. also, a variable of type int should be passed an int that will hold the index. returns a index by referense and a float with the smallest by return

---
+++ FIndSmallest.cpp
@@ -0,0 +1,18 @@
+
+#include <vector>
+using namespace std;
+
+int FindClosest(vector<float> myV,int& wineryNum)
+{
+    float smallest =0;
+    smallest = myV[0];
+    for(unsigned int index=1;index<myV.size();index++)
+    {
+        if(smallest>myV[index]&&myV[index]!=0.0)
+        {
+            smallest = myV[index];
+            wineryNum =index=1;
+        }
+    }
+    return smallest;
+}
